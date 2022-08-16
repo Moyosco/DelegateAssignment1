f28cd232e168b7f3b2e6516bc6b4612f0aae4aef
Support <Support@WEMA-TRAINING-S>
2022-08-16 10:43:21 +0100

    Add project files.

---
+++ DelegateAssignment1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DelegateAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        internal class Delegates1
+        {
+            public delegate bool Predicate(XClass x);
+
+            void LambdaExpansion()
+            {
+
+                var likelyUsername = "Oloruntobi";
+
+                Predicate classwork = (XClass x) =>
+                {
+                    if (x.UserName == likelyUsername)  //check if Username is equal to LikelyUsername
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                };
+            }
+        }
+       
+    }
+
+    class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
+    
